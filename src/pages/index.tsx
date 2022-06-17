888554fbc6c8017931b2da3a1b0d31251da49fed
infamous55 <infamouslegend555@gmail.com>
2022-06-17 19:46:25 +0300

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-6xl font-bold">Hello World!</h1>
+    </div>
+  )
+}
+
+export default Home
